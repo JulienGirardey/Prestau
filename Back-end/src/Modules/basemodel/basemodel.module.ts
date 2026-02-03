2ed0c50a0189549300a68886b4c0107c0742f019
JulienGirardey <juliengirardey2@gmail.com>
2026-02-03 11:24:12 +0100

    refactor: update Basemodel module by removing unused controller and entity

--- Back-end/src/Modules/basemodel/basemodel.module.ts
+++ Back-end/src/Modules/basemodel/basemodel.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { BasemodelService } from './basemodel.service';
-import { BasemodelController } from './basemodel.controller';
 import { PrismaService } from '../../prisma.service';
 
 @Module({
   providers: [BasemodelService, PrismaService],
-  controllers: [BasemodelController],
 })
 export class BasemodelModule {}
