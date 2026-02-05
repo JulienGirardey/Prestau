8e62230f9ec16833f4efd764bcc5e030e46e89dd
JulienGirardey <juliengirardey2@gmail.com>
2026-02-05 09:06:53 +0100

    refactor(worker): remove unnecessary fields from CreateWorkerDto and simplify UpdateWorkerDto

--- Back-end/src/Modules/worker/dto/update-worker.dto.ts
+++ Back-end/src/Modules/worker/dto/update-worker.dto.ts
@@ -1,20 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateWorkerDto } from './create-worker.dto';
 
-export class UpdateWorkerDto extends PartialType(CreateWorkerDto) {
-    firstName!: string;
-    lastName!: string;
-    city?: string;
-    postalCode?: string;
-    photoURL?: string;
-    profession?: string;
-    experience_years?: number;
-    languages?: string;
-    qualifications?: string;
-    cv_url?: string;
-    availability?: boolean;
-    phoneNumber?: string;
-    skills?: string;
-    latitude?: number;
-    longitude?: number;
-}
+export class UpdateWorkerDto extends PartialType(CreateWorkerDto) {}
