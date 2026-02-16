8c69a56b423275cf6888849c3b71facbd80303ea
NikkoAvardes <11460@holbertonstudents.com>
2026-02-16 10:55:24 +0100

    feat(joboffer): enhance job offer creation and retrieval functionality
    - Added new fields to CreateJobofferDto: jobId, workerId, startDate, and endDate with appropriate validation.
    - Updated JobofferService to include methods for creating, finding, and removing job offers using Prisma.
    - Integrated PrismaService into JobofferModule for database interactions.
    - Added Jest for testing with relevant scripts in package.json.
    - Updated tsconfig.json to include Jest types for improved type checking.

--- Back-end/src/Modules/joboffer/dto/create-joboffer.dto.ts
+++ Back-end/src/Modules/joboffer/dto/create-joboffer.dto.ts
@@ -1,6 +1,26 @@
-import { IsString } from "class-validator";
+import { Type } from "class-transformer";
+import { IsString, IsInt, IsOptional, IsDateString, IsPositive } from "class-validator";
 
 export class CreateJobofferDto {
-	@IsString()
-	contract_url?: string;
+    @IsInt()
+	@IsPositive()
+    jobId!: number;
+
+    @IsInt()
+	@IsPositive()
+    workerId!: number;
+
+    @IsString()
+    @IsOptional()
+    contract_url?: string;
+
+    @IsDateString()
+    @IsOptional()
+	@Type(() => Date)
+    startDate?: string;
+
+    @IsDateString()
+    @IsOptional()
+	@Type(() => Date)
+    endDate?: string;
 }
