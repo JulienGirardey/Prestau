47f45478bb1af7b06e182d22a41ea73ca304c8dd
NikkoAvardes <11460@holbertonstudents.com>
2026-02-20 11:18:42 +0100

    refactor(user): améliorer la structure des méthodes dans UserController et UserService

--- Back-end/src/Modules/user/user.service.ts
+++ Back-end/src/Modules/user/user.service.ts
@@ -7,7 +7,7 @@ import { CreateUserDto } from "./dto/create-user.dto";
 import { UpdateUserDto } from "./dto/update-user.dto";
 import { Users } from "@prisma/client";
 import { PrismaService } from "../../prisma.service";
-import { Role } from '../auth/enums/role.enum';
+import { Role } from "../auth/enums/role.enum";
 
 @Injectable()
 export class UserService {
@@ -56,9 +56,16 @@ export class UserService {
     return this.Prisma.users.update({
       where: { id },
       data: {
-					...updateUserDto,
-					role: updateUserDto.role as Role,
-				},
+        ...updateUserDto,
+        role: updateUserDto.role as Role,
+      },
+	  select: {
+		id: true,
+		email: true,
+		role: true,
+		createdAt: true,
+		updatedAt: true,
+	  },
     });
   }
 
