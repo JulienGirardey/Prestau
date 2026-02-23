a53b2c16c62d1b0aab08fde952e6053fd6c7b336
NikkoAvardes <11460@holbertonstudents.com>
2026-02-23 10:47:18 +0100

    feat(message): ajouter la gestion des utilisateurs dans le contrôleur de messages

--- Back-end/src/Modules/message/dto/create-message.dto.ts
+++ Back-end/src/Modules/message/dto/create-message.dto.ts
@@ -17,4 +17,4 @@ export class CreateMessageDto {
 	@IsInt()
 	receiverId!: number;
 
-};
+}
--- Back-end/src/Modules/message/message.controller.ts
+++ Back-end/src/Modules/message/message.controller.ts
@@ -1,24 +1,26 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Req, ParseIntPipe } from '@nestjs/common';
 import { MessageService } from './message.service';
 import { CreateMessageDto } from './dto/create-message.dto';
+import { JwtAuthGuard } from 'src/guards/jwt-auth.guard';
 
 @Controller('message')
+@UseGuards(JwtAuthGuard)
 export class MessageController {
   constructor(private readonly messageService: MessageService) {}
 
   @Post()
-  create(@Body() createMessageDto: CreateMessageDto) {
-    return this.messageService.create(createMessageDto);
+  create(@Body() createMessageDto: CreateMessageDto, @Req() req: CurentUserRequest) {
+    return this.messageService.create(createMessageDto, req.user.id);
   }
 
   @Get()
-  findAll() {
-    return this.messageService.findAll();
+  findAll(@Req() req: CurentUserRequest) {
+    return this.messageService.findAll(req.user.id);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.messageService.findOne(+id);
+  findOne(@Param('id', ParseIntPipe) id: number, @Req() req: CurentUserRequest) {
+    return this.messageService.findOne(id, req.user.id);
   }
 
 }
