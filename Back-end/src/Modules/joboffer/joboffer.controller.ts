c98396b410246ff7e125b5d1a8c4c400865a6143
JulienGirardey <juliengirardey2@gmail.com>
2026-02-23 16:14:23 +0100

    feat(joboffer): implement user association in job offer creation and add conflict handling

--- Back-end/src/Modules/joboffer/joboffer.controller.ts
+++ Back-end/src/Modules/joboffer/joboffer.controller.ts
@@ -1,14 +1,21 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, UseGuards, Req, ParseIntPipe } from '@nestjs/common';
 import { JobofferService } from './joboffer.service';
 import { CreateJobofferDto } from './dto/create-joboffer.dto';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { Role } from '../auth/enums/role.enum';
+import { Roles } from '../auth/decorators/roles.decorator';
+import { RolesGuard } from '../auth/guards/roles.guard';
+import { CurrentUserRequest } from '../auth/interfaces/jwt-payload.interface';
 
 @Controller('joboffer')
+@UseGuards(JwtAuthGuard, RolesGuard) // Appliquer le guard d'authentification JWT à toutes les routes de ce contrôleur
 export class JobofferController {
   constructor(private readonly jobofferService: JobofferService) {}
 
   @Post()
-  create(@Body() createJobofferDto: CreateJobofferDto) {
-    return this.jobofferService.create(createJobofferDto);
+  @Roles(Role.WORKER) // seulement un Worker peut créer une offre d'emploi
+  create(@Body() createJobofferDto: CreateJobofferDto, @Req() req: CurrentUserRequest) {
+    return this.jobofferService.create(createJobofferDto, req.user.id); // Associer l'offre d'emploi créée à l'utilisateur qui l'a créée (même token)
   }
 
   @Get()
@@ -17,12 +24,12 @@ export class JobofferController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.jobofferService.findOne(+id);
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.jobofferService.findOne(id);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.jobofferService.remove(+id);
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.jobofferService.remove(id);
   }
 }
--- Back-end/src/Modules/joboffer/joboffer.service.ts
+++ Back-end/src/Modules/joboffer/joboffer.service.ts
@@ -1,15 +1,27 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateJobofferDto } from './dto/create-joboffer.dto';
 import { PrismaService } from '../../prisma.service';
-import { JobOffer, Prisma, Review } from '@prisma/client';
+import { JobOffer } from '@prisma/client';
+import { worker } from 'node:cluster';
 
 @Injectable()
 export class JobofferService {
 	constructor(private Prisma: PrismaService) { }
 
-	async create(createJobofferDto: CreateJobofferDto): Promise<JobOffer> {
+	async create(createJobofferDto: CreateJobofferDto, userId: number): Promise<JobOffer> {
+		const existingJobOffer = await this.Prisma.jobOffer.findFirst({
+			where: { workerId: userId },
+		});
+
+		if (existingJobOffer) {
+			throw new NotFoundException('A job offer already exists for this worker.');
+		}
+
 		return this.Prisma.jobOffer.create({
-			data: createJobofferDto,
+			data: {
+				...createJobofferDto,
+				workerId: userId, // association de l'offre d'emploi avec l'utilisateur qui l'a créée (même token)
+			},
 		});
 	}
 
