62e9683abd2821578206f6284237532d0736638b
JulienGirardey <juliengirardey2@gmail.com>
2026-02-25 17:40:56 +0100

    feat: enhance DashboardCompany layout and update Header component styling

--- Front-end/Prestau/app/(tabs)/dashboard-company.tsx
+++ Front-end/Prestau/app/(tabs)/dashboard-company.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text } from "react-native";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useThemeColors } from "@/hooks/useThemeColors";
 import { Header } from "@/components/Header";
@@ -6,18 +6,20 @@ import { Header } from "@/components/Header";
 export default function DashboardCompany() {
     const colors = useThemeColors();
     return (
-        <SafeAreaView style={[styles.container, { backgroundColor: colors.background }]}>
-            <Header title="Dashboard Company" />
-            <Text style={styles.title}>Dashboard Company</Text>
-        </SafeAreaView>
+        <View style={{ flex: 1 }}>
+            <Header />
+            <SafeAreaView style={[styles.container, { backgroundColor: colors.background }]}>
+                <ScrollView style={styles.body}>
+                    <Text style={styles.title}>Dashboard Company</Text>
+                </ScrollView>
+            </SafeAreaView>
+        </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        alignItems: 'center',
-        padding: 100
+        flex: 1
     },
     title: {
         paddingTop: 30,
@@ -25,8 +27,7 @@ const styles = StyleSheet.create({
         textAlign: "center"
     },
     body: {
-        flex: 1,
-        justifyContent: "space-between"
+        flex: 1
     },
     form: {
         width: "100%"
