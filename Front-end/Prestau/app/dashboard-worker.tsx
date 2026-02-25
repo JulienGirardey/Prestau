841d14a8a66ed011e7d731ff9703a774ad525b3d
NikkoAvardes <11460@holbertonstudents.com>
2026-02-25 11:44:56 +0100

    feat: ajouter le composant DashboardWorker avec des cartes et un bouton

---
+++ Front-end/Prestau/app/dashboard-worker.tsx
@@ -0,0 +1,59 @@
+import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useThemeColors } from "@/hooks/useThemeColors";
+import { Header } from "@/components/Header";
+import { DefaultCard } from "@/components/DefaultCard";
+import { NewButton } from "@/components/Button";
+
+export default function DashboardWorker() {
+  const colors = useThemeColors();
+  const handlePress = () => {
+    console.log("Button clique");
+  };
+  return (
+    <SafeAreaView
+      style={[styles.container, { backgroundColor: colors.background }]}
+    >
+      <Header title="Dashboard Worker" />
+      <Text style={styles.title}>Dashboard Worker</Text>
+      <DefaultCard title="Mission a venir">
+        <Text>Mission a venir</Text>
+      </DefaultCard>
+      <DefaultCard title="Me disponibilites">
+        <Text>Me disponibilités</Text>
+      </DefaultCard>
+      <DefaultCard title="Dernières missions postées">
+        <Text>Dernières missions postées</Text>
+      </DefaultCard>
+      <View style={styles.buttonContainer}>
+        <NewButton title="Avis Professionels" onPress={handlePress}/>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    padding: 100,
+  },
+  title: {
+    paddingTop: 30,
+    fontSize: 31,
+    textAlign: "center",
+  },
+  body: {
+    flex: 1,
+    justifyContent: "space-between",
+  },
+  form: {
+    width: "100%",
+  },
+  spacer: {
+    flex: 1,
+  },
+  buttonCreateAccount: {
+    marginTop: 30,
+  },
+});
