e9ea47b3def6db36810af0b1d3fed20e051406c2
NikkoAvardes <11460@holbertonstudents.com>
2026-02-27 10:49:34 +0100

    refactor: restructure DashboardWorker component layout and styles

--- Front-end/Prestau/app/(tabs)/dashboard-worker.tsx
+++ Front-end/Prestau/app/(tabs)/dashboard-worker.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useThemeColors } from "@/hooks/useThemeColors";
 import { Header } from "@/components/Header";
@@ -6,52 +6,57 @@ import { DefaultCard } from "@/components/DefaultCard";
 import { NewButton } from "@/components/Button";
 
 export default function DashboardWorker() {
-  const colors = useThemeColors();
-  const handlePress = () => {
-    console.log("Button clique");
-  };
-  return (
-    <SafeAreaView
-      style={[styles.container, { backgroundColor: colors.background }]}
-    >
-      <Header title="Dashboard Worker" />
-      <Text style={styles.title}>Dashboard Worker</Text>
-      <DefaultCard title="Missions à venir">
-        {}
-        <Text style={styles.titletext}>Missions à venir</Text>
-      </DefaultCard>
-      <DefaultCard title="Me disponibilites">
-        <Text style={styles.titletext}>Mes disponibilités</Text>
-      </DefaultCard>
-      <DefaultCard title="Dernières missions postées">
-        <Text style={styles.titletext}>Dernières missions postées</Text>
-      </DefaultCard>
-      <View style={styles.buttonContainer}>
-        <NewButton title="Avis Professionnels" onPress={handlePress} />
-      </View>
-    </SafeAreaView>
-  );
+    const colors = useThemeColors();
+    return (
+        <View style={{ flex: 1 }}>
+            <Header />
+            <SafeAreaView style={[styles.container, { backgroundColor: colors.background }]}>
+                <ScrollView style={styles.body} contentContainerStyle={{ alignItems: "center", gap: 20,  }}>
+                        <Text style={styles.title}>Dashboard Worker</Text>
+					<DefaultCard title="Missions à venir">
+						<Text style={styles.titleCard}>Missions à venir</Text>
+						<DefaultCard title="Mission 1">
+							<Text style={styles.titleCard}>Mission 1</Text>
+						</DefaultCard>
+					</DefaultCard>
+					<DefaultCard title="Mes disponibilités">
+						<Text style={styles.titleCard}>Mes disponibilités</Text>
+					</DefaultCard>
+					<DefaultCard title="Dernières missions postées">
+						<Text style={styles.titleCard}>Dernières missions postées</Text>
+					</DefaultCard>
+					<View style={styles.buttonCreateAccount}>
+						<NewButton title="Avis Professionnels" onPress={() => console.log("Button clique")} />
+					</View>
+                </ScrollView>
+            </SafeAreaView>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    gap: 20,
-    alignItems: "center",
-    padding: 100,
-  },
-  title: {
-    paddingTop: 30,
-    fontSize: 31,
-    textAlign: "center",
-  },
-  buttonContainer: {
-    marginTop: 20,
-  },
-  titletext: {
-	fontSize: 18,
+    container: {
+        flex: 1
+    },
+    title: {
+        fontSize: 31,
+        textAlign: "center"
+    },
+    body: {
+        flex: 1
+    },
+    form: {
+        width: "100%"
+    },
+	buttonCreateAccount: {
+		marginTop: 20,
+		paddingBottom: 20,
+		width: "100%"
+	},
+	titleCard: {
+	fontSize: 25,
 	textAlign: "center",
 	color: "#F5F2D9",
-	marginTop: -25,
-  }
+	marginTop: -20
+	}
 });
