81c06002e1fab501c6a23aec0f585d3822dbeb9d
adi-mart <aurelie.di-martino@holbertonstudents.com>
2026-03-02 17:15:18 +0100

    feat: add logout functionality to auth controller
    - Added a `logout` endpoint to the AuthController.
    - Protected the `logout` route with `@UseGuards(JwtAuthGuard)` to ensure only authenticated users can access it.
    - The `logout` method retrieves the user ID from the JWT payload and delegates the logout logic to the AuthService.

--- Back-end/src/Modules/auth/auth.controller.ts
+++ Back-end/src/Modules/auth/auth.controller.ts
@@ -1,7 +1,10 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Req } from '@nestjs/common';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
 import { AuthService } from './auth.service'
+import { UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { CurrentUserRequest } from './interfaces/jwt-payload.interface';
 
 @Controller("auth")
 export class AuthController {
@@ -15,5 +18,11 @@ export class AuthController {
 	@Post("login") // un utilisateur doit pouvoir se connecter
 	login(@Body() loginDto: LoginDto) {
 		return this.authService.login(loginDto);
+	}
+
+	@Post('logout') // un utilisateur doit pouvoir se déconnecter
+	@UseGuards(JwtAuthGuard)
+	logout(@Req() req: CurrentUserRequest) {
+		return this.authService.logout(req.user.id);
 	}
 }
