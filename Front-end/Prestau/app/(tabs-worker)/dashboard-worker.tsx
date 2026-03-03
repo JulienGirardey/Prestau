5cdf93c45963d256e1d97377cad457ee6c6ae1e2
NikkoAvardes <11460@holbertonstudents.com>
2026-03-03 18:03:58 +0100

    feat: Ajouter le composant ProfileWorker pour afficher les informations du profil de l'utilisateur

--- Front-end/Prestau/app/(tabs-worker)/dashboard-worker.tsx
+++ Front-end/Prestau/app/(tabs-worker)/dashboard-worker.tsx
@@ -5,7 +5,7 @@ import { Header } from "@/components/Header";
 import { DefaultCard } from "@/components/DefaultCard";
 import { NewButton } from "@/components/Button";
 import { useState, useEffect, useMemo, useCallback } from "react";
-import { getJobs } from "@/src/api/job";
+import { getJobs, Job } from "@/src/api/job";
 import { Calendar, LocaleConfig } from 'react-native-calendars';
 import { getWorkerAvailability, updateWorkerAvailability } from "@/src/api/worker";
 
@@ -23,7 +23,7 @@ export default function DashboardWorker() {
     const colors = useThemeColors();
     
     // --- ÉTATS (STATES) ---
-    const [missions, setMissions] = useState<[]>([]);
+    const [missions, setMissions] = useState<Job[]>([]);
     const [isLoading, setIsLoading] = useState(true);
     
     // Les dates seront chargées depuis la base de données, tableaux vides par défaut
@@ -37,11 +37,11 @@ export default function DashboardWorker() {
             try {
                 // Chargement des missions
                 const jobsResponse = await getJobs();
-                setMissions(jobsResponse.data);
+                setMissions(jobsResponse || []);
 
                 const availabilityResponse = await getWorkerAvailability();
-                setFreeDays(availabilityResponse.data.freeDays);
-                setBusyDays(availabilityResponse.data.busyDays);
+                setFreeDays(availabilityResponse.data?.freeDays || []);
+                setBusyDays(availabilityResponse.data?.busyDays || []);
                 
             } catch (error) {
                 console.error("Erreur lors du chargement du dashboard:", error);
