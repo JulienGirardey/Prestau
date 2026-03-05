8968a08b03472c22de7e00ea6f7d31f33b379b46
NikkoAvardes <11460@holbertonstudents.com>
2026-03-05 10:49:54 +0100

    feat: Mettre à jour l'importation de la fonction pour récupérer les offres d'emploi et corriger son utilisation dans DashboardWorker

--- Front-end/Prestau/app/(tabs-worker)/dashboard-worker.tsx
+++ Front-end/Prestau/app/(tabs-worker)/dashboard-worker.tsx
@@ -7,7 +7,7 @@ import { NewButton } from "@/components/Button";
 import { useState, useEffect, useMemo, useCallback } from "react";
 import { Calendar, LocaleConfig } from 'react-native-calendars';
 import { getWorkerAvailability, updateWorkerAvailability } from "@/src/api/worker";
-import { getJobOffersByCompany, JobOffer } from "@/src/api/jobOffer";
+import { getMyJobOffers, JobOffer } from "@/src/api/joboffer";
 
 // CONFIGURATION DU CALENDRIER (LOCALISATION FR)
 LocaleConfig.locales['fr'] = {
@@ -36,7 +36,7 @@ export default function DashboardWorker() {
             setIsLoading(true);
             try {
                 // Chargement des missions
-                const jobsResponse = await getJobOffersByCompany();
+                const jobsResponse = await getMyJobOffers();
                 setMissions(jobsResponse || []);
 
                 const availabilityResponse = await getWorkerAvailability();
