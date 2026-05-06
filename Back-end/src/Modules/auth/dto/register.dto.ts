6bb377d4549637172fe8a0af0cbec80859631934
Adi-mart <aurelie.dimartino12@gmail.com>
2026-05-06 20:10:17 +0200

    fix: update Prisma client and package versions; enhance password validation in registration

--- Back-end/src/Modules/auth/dto/register.dto.ts
+++ Back-end/src/Modules/auth/dto/register.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsString, MinLength, IsEnum } from 'class-validator';
+import { IsEmail, IsString, MinLength, IsEnum, MaxLength, Matches } from 'class-validator';
 import { Role } from '../enums/role.enum';
 
 export class RegisterDto {
@@ -6,7 +6,12 @@ export class RegisterDto {
   email!: string;
 
   @IsString()
-  @MinLength(6) // Minimum length for password is 6 characters
+  @MinLength(8, { message: 'Le mot de passe doit contenir au moins 8 caractères' }) // Minimum length for password is 8 characters
+	@MaxLength(20, { message: 'Le mot de passe ne doit pas dépasser 20 caractères' }) // Maximum length for password is 20 characters
+	@Matches(/[^A-Za-z0-9]/, { message: 'Le mot de passe doit contenir au moins un caractère spécial' }) // Password must contain at least one special character
+	@Matches(/[A-Z]/, { message: 'Le mot de passe doit contenir au moins une lettre majuscule' }) // Password must contain at least one uppercase letter
+	@Matches(/[a-z]/, { message: 'Le mot de passe doit contenir au moins une lettre minuscule' }) // Password must contain at least one lowercase letter
+	@Matches(/[0-9]/, { message: 'Le mot de passe doit contenir au moins un chiffre' }) // Password must contain at least one digit
   password!: string;
 
   @IsEnum(Role) //WORKER or COMPANY
